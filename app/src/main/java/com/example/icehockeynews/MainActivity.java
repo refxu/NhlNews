e6b27d1c326cef3b3d488193b3a2f3632aa536f6
Richard Müller <rmu122668@stud.gibb.ch>
2021-07-07 13:19:18 +0200

    New File

--- app/src/main/java/com/example/icehockeynews/MainActivity.java
+++ app/src/main/java/com/example/icehockeynews/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.icehockeynews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.android.volley.RequestQueue;
+import com.example.icehockeynews.model.Teams;
 
 public class MainActivity extends AppCompatActivity {
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    private void addTeamsToClickableList(){
+        ListView teams = findViewById(R.id.teamsliste);
+        ArrayAdapter<Teams> teamsAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1);
     }
 }
